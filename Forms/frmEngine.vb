a81bfcd41cafd78481fc2f2820d2c8bfb4b984bf
tkarasc <tkarasch@sqdata.com>
2011-03-08 11:31:21 -0500

    RBC Release w/only 3.7.17 option

--- Forms/frmEngine.vb
+++ Forms/frmEngine.vb
@@ -513,6 +513,11 @@ doAgain:
             objThis.IncludeLib = txtIncludeLib.Text
             objThis.DTDLib = txtDTDLib.Text
             objThis.DDLLib = txtDDLLib.Text
+            'added for RBC   3/8/11
+            objThis.EngVersion = "3.7.17"
+
+
+
 
             Dim temp As Mylist = cbConn.SelectedItem
             'Dim answer As MsgBoxResult
