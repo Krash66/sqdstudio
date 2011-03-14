b75e6c930d75cd6496b91b9604be79fbd56b92f1
tkarasc <tkarasch@sqdata.com>
2011-03-14 13:13:51 -0400

    RBCReleaseUpdate/noSQData.exe

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -39,6 +39,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.3.2.8")> 
+<Assembly: AssemblyVersion("3.3.2.9")> 
 
-<Assembly: AssemblyFileVersionAttribute("3.3.2.8")> 
+<Assembly: AssemblyFileVersionAttribute("3.3.2.9")> 
