8a8f56cd15af3e7a60a6a7c2abf113a6700c6048
Krash66 <tomkarasch@tomtekdev.com>
2017-05-09 18:00:29 -0400

    Remove Redundant Backup Files
    Final Release

--- Backup/Forms/frmMultiMod.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class frmMultiMod
-
-    Private Sub tvModSel_AfterSelect(ByVal sender As System.Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles tvModSel.AfterSelect
-
-    End Sub
-
-End Class
--- Backup/Forms/frmODBCconnect.vb
+++
@@ -1,3 +0,0 @@
-Public Class frmODBCconnect
-
-End Class
